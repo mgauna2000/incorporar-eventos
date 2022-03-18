69fff8485949fcf16e1875afa25159445e4109cc
Martin <martingauna2000@gmail.com>
2022-03-17 23:21:02 -0300

    actualizacion

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 let form = document.getElementById("formulario");
+let inputNombre = document.getElementById("nombre"); //INPUT
+let inputApellidos = document.getElementById("apellidos"); //INPUT apellidos
+
+const textoNombre = document.getElementById("textoNombre"); //P
 
 form.addEventListener("submit", enviarFomulario);
 
 const enviaFormulario = (e) => {
     e.preventDefault();
+    console.log(`La persona se llama ${inputNombre.value} ${inputApellidos.value}`);
 }
