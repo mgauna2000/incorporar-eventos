8dbb76e6414dc1a40e3c961be543ca7efc97acd8
Martin <martingauna2000@gmail.com>
2022-03-18 14:27:45 -0300

    avanze

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 let form = document.getElementById("form");
 let inputName = document.getElementById("name"); 
 let inputEmail = document.getElementById("email"); 
-let inputPassword = document.getElementById("password")
+let inputPassword = document.getElementById("password");
+let inputCheck = document.getElementById("terms-and-cond");
+let inputNotification = document.getElementById("notification")
 
 const textoNombre = document.getElementById("textoNombre"); //P
 
-form.addEventListener("submit", enviarFormulario);
+form.addEventListener("submit", sendForm);
 
-function enviarFormulario(e) {
+function sendForm(e) {
     e.preventDefault();
-    console.log("Formulario Enviado");
+    console.log("Submitted form");
 }
